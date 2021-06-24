0805562dc432a6f86f424521f6f76b20489fa870
AntonioFuziy <antoniovf@al.insper.edu.br>
2021-06-24 12:04:54 -0300

    hello world test

--- functions/hello_world.js
+++ functions/hello_world.js
@@ -1,4 +1,6 @@
 exports.handler = function(context, event, callback) {
-  const result = { message : "Hello World!"}      
+  const result = { 
+    message : "Hello World!"
+  }      
   callback(null, result);   
 };
